4ac384b3df9c3b1c4691f1dce7387d163d6aea67
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-02 22:09:53 -0700

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from djang.contriib.dungon.models import Dungeon
 
-# Create your models here.
+class Post(models.Model):
+    ROOM_SHAPES = (
+        ('square', 'Square'),
+        ('octagon', 'Octagon'),
+    )
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    length = models.IntegerField()
+    width = models.IntegerField()
+    dungeon = models.ForeignKey(Dungeon,
+                                on_delete=models.CASCADE,
+                                related_name='dungeon_name')
+    shape = models.CharField(max_length=10,
+                                choices=ROOM_SHAPES,
+                                default='square')
+                                
+    class Meta:
+        order = ('-name')
+    
+    def __str__(self):
+        return self.name
