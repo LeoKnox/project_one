6c60e0dd51d209e3d49be63e67905683ffd7bd26
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-02 22:17:24 -0700

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from djang.contriib.dungon.models import Dungeon
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     ROOM_SHAPES = (
@@ -10,15 +10,15 @@ class Post(models.Model):
     description = models.TextField()
     length = models.IntegerField()
     width = models.IntegerField()
-    dungeon = models.ForeignKey(Dungeon,
+    dm = models.ForeignKey(User,
                                 on_delete=models.CASCADE,
-                                related_name='dungeon_name')
+                                related_name='dungeon_master')
     shape = models.CharField(max_length=10,
                                 choices=ROOM_SHAPES,
                                 default='square')
-                                
+
     class Meta:
-        order = ('-name')
+        ordering = ('-name',)
     
     def __str__(self):
         return self.name
