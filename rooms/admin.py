00d26ec3c42e699191f9bbde9df926104de092c1
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-03 22:32:53 -0700

    Add files via upload

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,7 +3,7 @@ from .models import Room
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'width', 'length', 'shape', 'dm', 'built')
+    list_display = ('name', 'description', 'width', 'length', 'shape', 'dm', 'built', 'rooms')
     list_filter = ('name', 'width', 'length', 'dm')
     search_fields = ('name', 'dm')
     ordering = ('name', 'dm')
