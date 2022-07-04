b0d1efbebca715637b38dc3bbf44c472c51236cc
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-03 18:10:47 -0700

    Add files via upload

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Room
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'width', 'length', 'shape', 'dm')
     list_filter = ('name', 'width', 'length', 'dm')
     search_fields = ('name', 'dm')
--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):
+class Room(models.Model):
     ROOM_SHAPES = (
         ('square', 'Square'),
         ('octagon', 'Octagon'),
